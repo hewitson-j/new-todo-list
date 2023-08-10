bf43d35e451bddc474aea0f66c5c57ad03c0e8f5
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-08-10 10:14:02 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppHeader from "./components/AppHeader";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+import {
+  ThemeProvider as MuiThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
+import { blue, yellow } from "@mui/material/colors";
+import { ReactNode } from "react";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: yellow[900],
+    },
+  },
+});
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export default function ThemeProvider(props: ThemeProviderProps) {
+  return <MuiThemeProvider theme={theme}>{props.children}</MuiThemeProvider>;
+}
